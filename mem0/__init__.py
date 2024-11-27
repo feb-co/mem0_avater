4ad77a7c3dbb6aaaa4fe0ad2885aef1ed17c12c5
abel <mingrui.xia.b@gmail.com>
2024-11-27 17:46:33 +0400

    merge main

--- mem0/__init__.py
+++
@@ -1,6 +0,0 @@
-import importlib.metadata
-
-__version__ = importlib.metadata.version("mem0_avater")
-
-from mem0.client.main import MemoryClient  # noqa
-from mem0.memory.memory import Memory  # noqa
